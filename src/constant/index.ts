ecb1b7bec84cd14c3aef67cc44dc1814b051de94
ethanparker <ethanparker0103@outlook.com>
2025-10-23 21:41:28 +0800

    feat

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,17 +1,17 @@
 export const isProd = false
 
 export const medias = {
-  telegram: 'https://t.me/AnteX_Official',
-  x: 'https://x.com/AntexAi',
-  twitter: 'https://x.com/AntexAi',
-  medium: 'https://medium.com/@antexai',
+  telegram: 'https://t.me/AntX_DEX',
+  x: 'https://x.com/AntX_Fi',
+  twitter: 'https://x.com/AntX_Fi',
+  medium: 'https://medium.com/@AntX_Fi',
   gitbook: 'https://docs.antex.ai/',
   linkTr: 'https://linktr.ee/antex.ai',
-  discord: 'https://discord.gg/AnteX',
+  discord: 'https://discord.gg/AntX',
 }
 
 export const env = {
-  SYSTEM_NAME: 'AnteX',
+  SYSTEM_NAME: 'AntX',
 }
 
 export const tradeUrl = isProd
@@ -20,22 +20,23 @@ export const tradeUrl = isProd
 
 // src/config/seo.ts
 export const SEO_CONFIG = {
-  title: 'AnteX | Fully On-Chain Perpetual Exchange with Orderbook & Native Oracle',
+  title:
+    'AntX | Fully On-Chain Perpetual Exchange with Orderbook & Native Oracle',
   description:
-    'AnteX is a fully decentralized perpetual exchange powered by an on-chain orderbook, oracle network, and cross-chain protocol. Trade with CEX-like performance, while keeping full custody of your assets.',
-  keywords: 'AnteX, AnteX Exchange, Decentralized Perpetual Exchange, Perp DEX, On-chain Orderbook, Native Oracle Network, Cross-chain Liquidity, Trade Crypto Perps, Self-Custody Crypto Trading',
+    'AntX is a fully decentralized perpetual exchange powered by an on-chain orderbook, oracle network, and cross-chain protocol. Trade with CEX-like performance, while keeping full custody of your assets.',
+  keywords:
+    'AntX, AntX Exchange, Decentralized Perpetual Exchange, Perp DEX, On-chain Orderbook, Native Oracle Network, Cross-chain Liquidity, Trade Crypto Perps, Self-Custody Crypto Trading',
   url: 'https://antex.ai/',
-  image:
-    'https://antex.ai/poster/AnteX_poster.png',
+  image: 'https://antex.ai/poster/AnteX_poster.png',
   type: 'website',
   locale: 'en_US',
   twitter: {
     card: 'summary_large_image',
-    site: 'https://antex.ai/',
-    creator: '@AntexAi',
+    site: 'https://x.com/AntX_Fi',
+    creator: '@AntX_Fi',
   },
   apple: {
-    appName: 'AnteX',
+    appName: 'AntX',
     appId: 'com.antex.app',
   },
   robots: 'index, follow',
